7e2fc5b5726423060e463c660c969a78bcbbf999
bwqr <ruzgardeniz.08@hotmail.com>
2020-04-10 18:06:26 +0300

    Imageable now uses Weightable trait

--- ImageableHelper.php
+++ ImageableHelper.php
@@ -4,6 +4,7 @@
 namespace App\Modules\Image;
 
 
+use App\Modules\Core\Traits\Weightable;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -11,6 +12,8 @@ use ImageFactory;
 
 class ImageableHelper
 {
+    use Weightable;
+
     public function deleteImages(array $images)
     {
         Imageable::whereIn('image', $images)->delete();
@@ -20,34 +23,9 @@ class ImageableHelper
         }
     }
 
-    public function updateWeights(array $imageIdsAndWeights)
+    public function updateImagesWeights(array $imageIdsAndWeights)
     {
-        $table = Imageable::getModel()->getTable();
-
-        $cases = [];
-        $ids = [];
-        $params = [];
-
-        foreach ($imageIdsAndWeights as $value) {
-            if (!array_key_exists('id', $value) || !array_key_exists('weight', $value)) {
-                abort(422);
-            }
-
-            $id = (int)$value['id'];
-            $cases[] = "WHEN {$id} then ?";
-            $params[] = $value['weight'];
-            $ids[] = $id;
-        }
-
-        $ids = implode(',', $ids);
-
-        $cases = implode(' ', $cases);
-
-        $params[] = Carbon::now();
-
-        DB::update("UPDATE `{$table}` SET `weight` = CASE `id` {$cases} END, `updated_at` = ? WHERE `id` in ({$ids})", $params);
-
-        return response()->json();
+        $this->updateWeights(Imageable::getModel()->getTable(), $imageIdsAndWeights);
     }
 
     public function storeImageFile($imageFile)
